e5e7b9598f7ba242e2b12e35962db2c5da426960
Zachary Stufflebeme <Stufflebeme.Zachary@gmail.com>
2023-01-11 14:52:21 -0600

    edit

---
+++ zach_docs/Acquire.py
@@ -0,0 +1,107 @@
+import json
+from typing import Dict, List, Union, cast
+import requests
+from bs4 import BeautifulSoup
+
+from env import github_token, github_username
+
+headers = {"Authorization": f"token {github_token}", "User-Agent": github_username}
+
+if headers["Authorization"] == "token " or headers["User-Agent"] == "":
+    raise Exception(
+        "You need to follow the instructions marked TODO in this script before trying to use it"
+    )
+
+
+def github_api_request(url: str) -> Union[List, Dict]:
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+    if response.status_code != 200:
+        raise Exception(
+            f"Error response from github api! status code: {response.status_code}, "
+            f"response: {json.dumps(response_data)}"
+        )
+    return response_data
+
+
+def get_repo_language(repo: str) -> str:
+    url = f"https://api.github.com/repos/{repo}"
+    repo_info = github_api_request(url)
+    if type(repo_info) is dict:
+        repo_info = cast(Dict, repo_info)
+        if "language" not in repo_info:
+            raise Exception(
+                "'language' key not round in response\n{}".format(json.dumps(repo_info))
+            )
+        return repo_info["language"]
+    raise Exception(
+        f"Expecting a dictionary response from {url}, instead got {json.dumps(repo_info)}"
+    )
+
+
+def get_repo_contents(repo: str) -> List[Dict[str, str]]:
+    url = f"https://api.github.com/repos/{repo}/contents/"
+    contents = github_api_request(url)
+    if type(contents) is list:
+        contents = cast(List, contents)
+        return contents
+    raise Exception(
+        f"Expecting a list response from {url}, instead got {json.dumps(contents)}"
+    )
+
+
+def get_readme_download_url(files: List[Dict[str, str]]) -> str:
+    """
+    Takes in a response from the github api that lists the files in a repo and
+    returns the url that can be used to download the repo's README file.
+    """
+    for file in files:
+        if file["name"].lower().startswith("readme"):
+            return file["download_url"]
+    return ""
+
+
+def process_repo(repo: str) -> Dict[str, str]:
+    """
+    Takes a repo name like "gocodeup/codeup-setup-script" and returns a
+    dictionary with the language of the repo and the readme contents.
+    """
+    contents = get_repo_contents(repo)
+    readme_download_url = get_readme_download_url(contents)
+    if readme_download_url == "":
+        readme_contents = ""
+    else:
+        readme_contents = requests.get(readme_download_url).text
+    return {
+        "repo": repo,
+        "language": get_repo_language(repo),
+        "readme_contents": readme_contents,
+    }
+
+
+
+def scrape_github_data(REPOS) -> List[Dict[str, str]]:
+    """
+    Loop through all of the repos and process them. Returns the processed data.
+    """
+    return [process_repo(repo) for repo in REPOS]
+
+
+if __name__ == "__main__":
+    data = scrape_github_data()
+    json.dump(data, open("../nadia_docs/data.json", "w"), indent=1)
+
+def get_repo_links():
+    start_link = 'https://github.com/search?q=space&type=Repositories'
+    headers = {"Authorization": f"token {github_token}", "User-Agent": github_username}
+    response = requests.get(start_link, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    #languages = ['JavaScript', 'Python', 'C#', 'Java']#
+    test_links = []
+    #filename = 'repos.json'
+    all_blocks = soup.find_all('li', class_='repo-list-item hx_hit-repo d-flex flex-justify-start py-4 public source')
+    for block in all_blocks:
+        #language = block.find('span', itemprop='programmingLanguage').text
+        link = block.find('a', class_='v-align-middle')['href'][1:]
+        test_links.append(link)
+    return test_links
---
+++ zach_docs/env.py
@@ -0,0 +1,5 @@
+host = 'data.codeup.com' 
+user = 'leavitt_1872'
+password = 'arV3dFWpZeOnEmvIdxszjVfyMKCTVy1J'
+github_username = 'Zachary-Stufflebeme'
+github_token = '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'
