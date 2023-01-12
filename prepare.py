492eae2edeb5e3560073091b732050e1dc815344
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-01-12 10:29:32 -0600

    add imports

--- prepare.py
+++ prepare.py
@@ -6,6 +6,10 @@ import nltk
 from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 import warnings
 warnings.filterwarnings('ignore')
 
